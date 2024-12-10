3d5e4e14611c9d16052dfab7d3c173569cd1022b
pamenon <pamenon@redhat.com>
2024-12-10 22:55:21 +0000

    Added post and rest samples

---
+++ rest-api-sample/src/test/java/org/pamenon/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.pamenon;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GetSampleIT extends GetSampleTest {
+    // Execute the same tests but in packaged mode.
+}
